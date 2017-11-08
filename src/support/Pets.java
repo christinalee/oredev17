6c6ebe511cbe952de6e5b6f2acbfe058f3ec1ff8
Christina Lee <lee@pinterest.com>
2017-11-08 11:15:13 +0100

    Initial commit of DCNYC files
    Plus a little bit of cleanup

---
+++ src/support/Pets.java
@@ -0,0 +1,18 @@
+package support;
+
+class JavaPet<T> {
+    public T newPet() {
+        return null;
+    }
+}
+
+class Example {
+    void example() {
+        JavaPet<? extends Dog> doggo; // Use site variance
+        JavaPet<SmallDog> smallDog = new JavaPet<>();
+
+        doggo = smallDog;
+    }
+}
+
+
